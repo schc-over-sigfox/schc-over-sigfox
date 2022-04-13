a4aaf54fcb314f6d2e0862de6add7dc744fe69d8
Diego Wistuba <wistuba@niclabs.com>
2022-04-13 18:15:57 -0400

    Updating exceptions

--- Entities/exceptions.py
+++ Entities/exceptions.py
@@ -1,40 +1,38 @@
 class SCHCError(Exception):
+    """Base class for other exceptions"""
     pass
 
 
 class LengthMismatchError(SCHCError):
+    """Raised when the value of any SCHC field, object or structure doesn't match its expected size."""
     pass
 
 
 class SCHCTimeoutError(SCHCError):
+    """Raised when any timer (SCHC sender or receiver) expires."""
     pass
 
 
 class RuleSelectionError(SCHCError):
-    # print("ERROR: The SCHC packet cannot be fragmented in 2 ** M * WINDOW_SIZE fragments or less. A Rule ID cannot be "
-    #       "selected.")
+    """Raised when a Rule ID cannot be selected according to the SCHC Profile."""
     pass
 
 
 class SenderAbortError(SCHCError):
+    """Raised when a SCHC Sender-Abort message is sent or received."""
     pass
 
 
 class ReceiverAbortError(SCHCError):
-    pass
-
-
-class UnrequestedACKError(SCHCError):
+    """Raised when a SCHC Receiver-Abort message is sent or received."""
     pass
 
 
 class BadProfileError(SCHCError):
+    """Raised when the behavior of the SCHC Sender or Receiver does not match the behavior specified by the profile."""
     pass
 
 
 class NetworkDownError(SCHCError):
-    pass
-
-
-class FormatError(SCHCError):
+    """Raised when the network is assumed to be unable to transmit data."""
     pass
