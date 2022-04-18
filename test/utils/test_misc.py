fea05f9b95e27aba54a9a859a38bdbfbc9742347
Diego Wistuba <wistuba@niclabs.com>
2022-04-18 16:23:14 -0400

    Added misc functions

---
+++ test/utils/test_misc.py
@@ -0,0 +1,103 @@
+import unittest
+
+from utils.misc import *
+
+
+class TestMisc(unittest.TestCase):
+
+    def test_zfill(self):
+        s = "test"
+        z = zfill(s, 9)
+
+        self.assertEqual("00000test", z)
+
+        w = zfill(s, 2)
+        self.assertEqual(s, w)
+
+    def test_zpad(self):
+        s = "test"
+        z = zpad(s, 9)
+
+        self.assertEqual("test00000", z)
+
+        w = zpad(s, 2)
+        self.assertEqual(s, w)
+
+    def test_replace_char(self):
+        s = "1001111"
+        z = replace_char(s, 2, '1')
+
+        self.assertEqual("1011111", z)
+
+        s = "1001111"
+        z = replace_char(s, 10, '1')
+
+        self.assertEqual(s + '1', z)
+
+    def test_find(self):
+        s = "1001011"
+        o = find(s, '1')
+
+        self.assertEqual([0, 3, 5, 6], o)
+
+        s = "0000000"
+        o = find(s, '1')
+
+        self.assertEqual([], o)
+
+    def test_is_monochar(self):
+        s = "1111111"
+
+        self.assertTrue(is_monochar(s))
+        self.assertTrue(is_monochar(s), '1')
+        self.assertFalse(is_monochar(s, '0'))
+
+        s = ''
+
+        self.assertFalse(is_monochar(s))
+
+    def test_section_string(self):
+        s = "AAAABBCCCDDDDDD"
+        idx = [0, 4, 6, 9]
+
+        sections = section_string(s, idx)
+
+        self.assertEqual(["AAAA", "BB", "CCC", "DDDDDD"], sections)
+
+    def test_generate_packet(self):
+        s = generate_packet(40)
+
+        self.assertEqual(40, len(s))
+
+        _ = generate_packet(1000, 'debug/packet')
+
+        with open("debug/packet", 'r') as f:
+            p = f.read()
+
+        self.assertEqual(1000, len(p))
+
+        os.remove('debug/packet')
+
+    def test_invert_dict(self):
+        d = {
+            "a": 1,
+            "b": 2,
+            "c": 3
+        }
+
+        b = invert_dict(d)
+
+        self.assertEqual({
+            1: 'a',
+            2: 'b',
+            3: 'c'
+        }, b)
+
+        with self.assertRaises(ValueError):
+            d = {
+                "a": 1,
+                "b": 2,
+                "c": 2
+            }
+
+            b = invert_dict(d)
---
+++ utils/misc.py
@@ -0,0 +1,69 @@
+"""Module of miscellaneous functions used in the project."""
+import os
+
+
+def zfill(s: str, length: int) -> str:
+    """Adds zeroes at the begginning of a string until it completes the desired length."""
+    return '0' * (length - len(s)) + s
+
+
+def zpad(s: str, length: int) -> str:
+    """Adds zeroes at the end of a string until it completes the desired length."""
+    return s + '0' * (length - len(s))
+
+
+def replace_char(s: str, position: int, new_char: str) -> str:
+    """Replaces a single character in the specified position of a string."""
+    return "{p}{n}{s}".format(
+        p=s[:position],
+        n=new_char,
+        s=s[position + 1:]
+    )
+
+
+def find(s: str, char: str) -> 'list[int]':
+    """Lists all the occurrences of a character in a string."""
+    return [i for i, c in enumerate(s) if char == c]
+
+
+def is_monochar(s: str, char: str = None) -> bool:
+    """Checks if a string contains only repetitions of one character."""
+    monochar = len(set(s)) == 1
+
+    if char is not None:
+        return monochar and s[0] == char
+
+    return monochar
+
+
+def section_string(s: str, indices: list[int]) -> list[str]:
+    """Sections a string in the specified indices and returns a list of its sections."""
+    indices.append(len(s))
+
+    return [s[i:j] for i, j in zip(indices, indices[1:])]
+
+
+def generate_packet(byte_size: int, path: str = None) -> str:
+    """Generates a string of the specified byte size and optionally saves it into a file."""
+
+    s = '0'
+    i = 0
+    while len(s) < byte_size:
+        i = (i + 1) % 10
+        s += str(i)
+
+    if path is not None and not os.path.isfile(path):
+        with open(path, 'w') as f:
+            f.write(s)
+
+    return s
+
+
+def invert_dict(d: dict) -> dict:
+    """Inverts {key: value} pairs of a dictionary into {value: kay} pairs, only if no values are repeated."""
+    values = list(d.values())
+
+    if len(values) != len(set(values)):
+        raise ValueError("Dictionary cannot be inverted")
+
+    return {v: k for k, v in d.items()}
