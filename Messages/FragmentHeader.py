2458b674d82dc1057d9db71a3ae62f8234da7d29
Diego Wistuba <wistuba@niclabs.com>
2022-04-29 12:57:02 -0400

    Updated FragmentHeader class

--- Messages/FragmentHeader.py
+++ Messages/FragmentHeader.py
@@ -1,18 +1,52 @@
-from Entities.exceptions import LengthMismatchError
+from Entities.SigfoxProfile import SigfoxProfile
+from Entities.exceptions import LengthMismatchError, BadProfileError
 from Messages.Header import Header
+from config.schc import L2_WORD_SIZE
+from utils.casting import bin_to_bytes
+from utils.misc import is_monochar
 
 
 class FragmentHeader(Header):
-    FCN = ''
-    FRAGMENT_NUMBER = None
 
-    def __init__(self, profile, rule_id, dtag, w, fcn):
-        super().__init__(profile, rule_id, dtag, w)
+    def __init__(
+            self,
+            profile: SigfoxProfile,
+            dtag: str,
+            w: str,
+            fcn: str,
+            rcs: str = None
+    ):
+        """Subclass of Header, used exclusively in Fragments."""
+        super().__init__(profile, dtag, w)
 
         if len(fcn) != profile.N:
             raise LengthMismatchError("FCN must be of length N")
         else:
             self.FCN = fcn
 
-    def to_string(self):
-        return "".join([super().to_string(), self.FCN])
+        if rcs is None:
+            self.RCS = ''
+        else:
+            if profile.U != 0 and len(rcs) != profile.U:
+                raise LengthMismatchError("RCS must be of length U")
+            elif not is_monochar(self.FCN, '1'):
+                raise BadProfileError("RCS was not None in a regular fragment (not All-1)")
+            else:
+                self.RCS = rcs
+
+        self.PADDING = ''
+        header_length = len(self.RULE_ID + self.DTAG + self.W + self.FCN + self.RCS) % L2_WORD_SIZE
+
+        if header_length % L2_WORD_SIZE != 0:
+            self.PADDING = '0' * (L2_WORD_SIZE - (header_length % L2_WORD_SIZE))
+
+    def to_binary(self) -> str:
+        """Generate the binary string representation of the Header"""
+
+        fields_in_order = [self.RULE_ID, self.DTAG, self.W, self.FCN, self.RCS, self.PADDING]
+        return ''.join(fields_in_order)
+
+    def to_bytes(self) -> bytes:
+        """Generate the byte representation of the Header,"""
+
+        return bin_to_bytes(self.to_binary())
--- Messages/Header.py
+++ Messages/Header.py
@@ -1,20 +1,23 @@
+from Entities.SigfoxProfile import SigfoxProfile
 from Entities.exceptions import LengthMismatchError
 
 
 class Header:
-    PROFILE = None
-    RULE_ID = ""
-    DTAG = ""
-    W = ""
-    WINDOW_NUMBER = None
 
-    def __init__(self, profile, rule_id, dtag, w):
-        self.profile = profile
+    def __init__(
+            self,
+            profile: SigfoxProfile,
+            dtag: str,
+            w: str
+    ):
+        """Class to encapsulate SCHC Header fields present in Fragments and ACKs."""
 
-        if len(rule_id) != profile.RULE_ID_SIZE:
+        self.PROFILE = profile
+
+        if len(str(profile.RULE)) != profile.RULE_ID_SIZE:
             raise LengthMismatchError("RULE must be of length RULE_ID_SIZE")
         else:
-            self.RULE_ID = rule_id
+            self.RULE_ID = str(profile.RULE)
 
         if profile.T == "0":
             self.DTAG = ""
@@ -29,9 +32,6 @@ class Header:
             self.W = w
             self.WINDOW_NUMBER = int(self.W, 2)
 
-    def to_string(self):
-        return "".join([self.RULE_ID, self.DTAG, self.W])
-
-    def to_bytes(self):
-        s = self.to_string()
-        return bytes(int(s[i:i + 8], 2) for i in range(0, len(s), 8))
+    def to_binary(self) -> str:
+        """Method to be implemented in child classes."""
+        raise NotImplementedError
