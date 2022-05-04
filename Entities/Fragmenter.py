31b4d0ea78189eaa9a528c1a98f0e39a29d3b33b
Diego Wistuba <wistuba@niclabs.com>
2022-05-04 15:06:51 -0400

    Updated Fragmenter class

--- Entities/Fragmenter.py
+++ Entities/Fragmenter.py
@@ -1,63 +1,106 @@
-from math import ceil, floor
+import json
+from math import floor
 
+from Entities.SigfoxProfile import SigfoxProfile
+from Entities.exceptions import LengthMismatchError
 from Messages.Fragment import Fragment
+from Messages.FragmentHeader import FragmentHeader
+from db.CommonFileStorage import CommonFileStorage as Storage
+from utils.casting import int_to_bin, bytes_to_bin
 from utils.schc_utils import zfill
 
 
 class Fragmenter:
+    """A Fragmenter object, which generates SCHC Fragments from a SCHC Packet."""
 
-    def __init__(self, profile, schc_packet):
+    def __init__(
+            self,
+            profile: SigfoxProfile,
+            fragment_dir: str = "debug/sd",
+    ):
+        """
+        Instantiate a Fragmenter.
+
+        Args:
+            profile: (SCHCProfile) SCHC Profile used for the fragmentation procedure.
+            fragment_dir: (str) Directory where to store the fragments,
+        """
         self.PROFILE = profile
-        self.SCHC_PACKET = schc_packet
+        self.STORAGE = Storage(f"{fragment_dir}/rule_{self.PROFILE.RULE.ID}")
         self.CURRENT_FRAGMENT_NUMBER = 0
-        self.FRAGMENT_LIST = []
 
-    def fragment(self):
-        payload_max_length = int((self.PROFILE.UPLINK_MTU - self.PROFILE.HEADER_LENGTH) / 8)
-        message = self.SCHC_PACKET
-        n = self.PROFILE.N
-        m = self.PROFILE.M
-        number_of_fragments = int(ceil(float(len(message)) / payload_max_length))
+        if not self.STORAGE.folder_exists("fragments"):
+            self.STORAGE.create_folder("fragments")
 
-        print("[FRGM] Fragmenting message into " + str(number_of_fragments) + " pieces...")
+    def generate_fragment(self, payload: bytes, all_1: bool):
+
+        number_of_windows = 2 ** self.PROFILE.M
+        w = int_to_bin(
+            floor(self.CURRENT_FRAGMENT_NUMBER / self.PROFILE.WINDOW_SIZE % number_of_windows), self.PROFILE.M
+        )
+        dtag = ''
+
+        if all_1:
+            fcn = '1' * self.PROFILE.N
+            rcs = int_to_bin(self.CURRENT_FRAGMENT_NUMBER % self.PROFILE.WINDOW_SIZE + 1, self.PROFILE.U)
+            header_length = self.PROFILE.RULE.ALL1_HEADER_LENGTH
+            payload_max_length = self.PROFILE.UPLINK_MTU - header_length
+        else:
+            index = self.PROFILE.WINDOW_SIZE - (self.CURRENT_FRAGMENT_NUMBER % self.PROFILE.WINDOW_SIZE) - 1
+            fcn = int_to_bin(index, self.PROFILE.N)
+            rcs = None
+            header_length = self.PROFILE.RULE.HEADER_LENGTH
+            payload_max_length = self.PROFILE.UPLINK_MTU - header_length
+
+        header = FragmentHeader(self.PROFILE, dtag, w, fcn, rcs)
+
+        if len(header.to_binary()) > header_length:
+            raise LengthMismatchError(
+                f"Header is larger than its maximum size ({len(header.to_binary())} > {header_length})."
+            )
+        if len(bytes_to_bin(payload)) > payload_max_length:
+            raise LengthMismatchError(
+                f"Payload is larger than its maximum size {(len(bytes_to_bin(payload)))} > {payload_max_length}."
+            )
+
+        fragment = Fragment(self.PROFILE, header, payload)
+        fragment_data = {
+            "hex": fragment.to_hex(),
+            "sent": False
+        }
+        window_index = zfill(
+            str(fragment.HEADER.WINDOW_NUMBER), (number_of_windows - 1) // 10 + 1
+        )
+        fragment_index = zfill(
+            str(fragment.INDEX), self.PROFILE.WINDOW_SIZE // 10 + 1
+        )
+        self.STORAGE.write(
+            f"fragments/fragment_w{window_index}f{fragment_index}", json.dumps(fragment_data)
+        )
+        self.CURRENT_FRAGMENT_NUMBER = (self.CURRENT_FRAGMENT_NUMBER + 1) % self.PROFILE.MAX_FRAGMENT_NUMBER
+
+        return fragment
+
+    def fragment(self, schc_packet: bytes) -> list[Fragment]:
+        """
+        Generates a list of SCHC Fragments.
+        """
+
+        payload_max_length = (self.PROFILE.UPLINK_MTU - self.PROFILE.RULE.HEADER_LENGTH) // 8
+        number_of_fragments = -(len(schc_packet) // -payload_max_length)
+        fragments = []
+
+        if len(schc_packet) % payload_max_length == 0:
+            number_of_fragments += 1
 
         for i in range(number_of_fragments):
-            self.CURRENT_FRAGMENT_NUMBER = i
-            payload = message[i * payload_max_length:(i + 1) * payload_max_length]
-            f = self.generate_fragment(payload, number_of_fragments)
-            self.FRAGMENT_LIST.append(f)
+            payload = schc_packet[i * payload_max_length:(i + 1) * payload_max_length]
+            fragment = self.generate_fragment(payload, all_1=(i == number_of_fragments - 1))
+            fragments.append(fragment)
 
         self.CURRENT_FRAGMENT_NUMBER = 0
+        return fragments
 
-        return self.FRAGMENT_LIST
-
-    def generate_fragment(self, payload, number_of_fragments=None):
-        if number_of_fragments is None:
-            number_of_fragments = self.PROFILE.MAX_FRAGMENT_NUMBER
-
-        payload_max_length = int((self.PROFILE.UPLINK_MTU - self.PROFILE.HEADER_LENGTH) / 8)
-        n = self.PROFILE.N
-        m = self.PROFILE.M
-        w = zfill(bin(int(floor((self.CURRENT_FRAGMENT_NUMBER / (2 ** n - 1) % (2 ** m)))))[2:], m)
-        fcn = zfill(bin((2 ** n - 2) - (self.CURRENT_FRAGMENT_NUMBER % (2 ** n - 1)))[2:], n)
-
-        if self.SCHC_PACKET is None or len(self.SCHC_PACKET) <= 300:
-            rule_id = "000"
-            dtag = ""
-            if len(payload) < payload_max_length or self.CURRENT_FRAGMENT_NUMBER == number_of_fragments - 1:
-                fcn = "111"
-            else:
-                fcn = zfill(bin((2 ** n - 2) - (self.CURRENT_FRAGMENT_NUMBER % (2 ** n - 1)))[2:], n)
-        else:
-            rule_id = "11110000"
-            dtag = ""
-            if len(payload) < payload_max_length or self.CURRENT_FRAGMENT_NUMBER == number_of_fragments - 1:
-                fcn = "11111"
-            else:
-                fcn = zfill(bin((2 ** n - 2) - (self.CURRENT_FRAGMENT_NUMBER % (2 ** n - 1)))[2:], n)
-
-        h = rule_id + dtag + w + fcn
-        header = bytes(int(h[i:i + 8], 2) for i in range(0, len(h), 8))
-
-        self.CURRENT_FRAGMENT_NUMBER += 1
-        return Fragment(self.PROFILE, [header, payload])
+    def clear_fragment_directory(self):
+        for file in self.STORAGE.list_files("fragments"):
+            self.STORAGE.delete_file(f"fragments/{file}")
