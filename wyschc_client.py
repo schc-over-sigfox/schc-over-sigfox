86e071486ae9011ee611747e1c22b00dba628ca2
Diego Wistuba <diegoswlt@gmail.com>
2022-09-27 20:56:11 -0300

    setting different packet experiments

--- wyschc_client.py
+++ wyschc_client.py
@@ -5,25 +5,33 @@ from Entities.SCHCSender import SCHCSender
 from Entities.SigfoxProfile import SigfoxProfile
 from utils.misc import generate_packet
 
-PACKET = generate_packet(256)
 
+sizes = [11, 54, 96, 139, 181, 224, 266, 308]
 loss_rates = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
 
-for repetition in range(100):
-    for lr in loss_rates:
-        print(f"LOSS RATE = {lr}")
-        print(f"(Repetition {repetition})")
-        profile = SigfoxProfile("UPLINK", "ACK ON ERROR", Rule('000'))
-        sender = SCHCSender(profile)
+for size in sizes:
 
-        sender.UPLINK_LOSS_RATE = lr
-        sender.PROFILE.SIGFOX_DL_TIMEOUT = 2
-        sender.PROFILE.RETRANSMISSION_TIMEOUT = 5
-        sender.ENABLE_MAX_ACK_REQUESTS = False
+    PACKET = generate_packet(size)
 
-        sender.start_session(PACKET)
+    for repetition in range(0, 1000):
+        for lr in loss_rates:
+            print(f"PACKET SIZE = {size}")
+            print(f"LOSS RATE = {lr}")
+            print(f"(Repetition {repetition})")
+            profile = SigfoxProfile("UPLINK", "ACK ON ERROR", Rule('000'))
+            sender = SCHCSender(profile)
 
-        sender.LOGGER.export(f"{str(lr).zfill(2)}_"
-                             f"rep{str(repetition).zfill(3)}")
+            sender.UPLINK_LOSS_RATE = lr
+            sender.PROFILE.SIGFOX_DL_TIMEOUT = 1
+            sender.PROFILE.RETRANSMISSION_TIMEOUT = 1
+            # sender.ENABLE_MAX_ACK_REQUESTS = False
+
+            sender.start_session(PACKET)
+
+            sender.LOGGER.export(
+                f"size_{size}_"
+                f"{str(lr).zfill(2)}_"
+                f"rep{str(repetition).zfill(3)}"
+            )
 
 print("All experiments complete")
