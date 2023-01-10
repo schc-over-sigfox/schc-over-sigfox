257856545f01934fd83af860edfc22f35c8829a2
Diego Wistuba <wistuba@niclabs.com>
2023-01-10 18:11:32 -0300

    removing unnecesary functions

--- test/utils/test_casting.py
+++ test/utils/test_casting.py
@@ -1,8 +1,8 @@
 import unittest
 
-from utils.casting import bin_to_int, bin_to_hex, bin_to_bytes, bin_to_string, \
+from utils.casting import bin_to_int, bin_to_hex, bin_to_bytes, \
     hex_to_bin, hex_to_bytes, bytes_to_bin, \
-    bytes_to_hex, int_to_bin, int_to_hex, int_to_bytes, string_to_bin
+    bytes_to_hex, int_to_bin, int_to_hex, int_to_bytes
 
 
 class TestCasting(unittest.TestCase):
@@ -20,11 +20,6 @@ class TestCasting(unittest.TestCase):
     def test_bin_to_bytes(self):
         b = '11010010'
         self.assertEqual(b'\xd2', bin_to_bytes(b))
-
-    def test_bin_to_string(self):
-        b = '01110100011001010111001101110100'
-        self.assertEqual('test', bin_to_string(b, 'utf-8'))
-        self.assertEqual('test', bin_to_string(b, 'ascii'))
 
     def test_hex_to_bin(self):
         h = 'cafe'
@@ -57,7 +52,3 @@ class TestCasting(unittest.TestCase):
     def test_int_to_bytes(self):
         n = 210
         self.assertEqual(b'\xd2', int_to_bytes(n))
-
-    def test_string_to_bin(self):
-        s = 'test'
-        self.assertEqual('01110100011001010111001101110100', string_to_bin(s))
