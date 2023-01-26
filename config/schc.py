8a758970d72ac5e11bd6269d3e56b069e8f25d50
Diego Wistuba <wistuba@niclabs.cl>
2023-01-26 15:42:19 -0300

    Setting default values

--- config/schc.py
+++ config/schc.py
@@ -1,17 +1,31 @@
+"""Contains global variables that control SCHC transmissions."""
+
 FR_MODE = "ACK ON ERROR"
-RETRANSMISSION_TIMEOUT = 100  # in seconds
-SIGFOX_DL_TIMEOUT = 60  # in seconds
-INACTIVITY_TIMEOUT = 500  # in seconds
+RETRANSMISSION_TIMEOUT = 43200  # in seconds
+SIGFOX_DL_TIMEOUT = 43200  # in seconds
+INACTIVITY_TIMEOUT = 43200  # in seconds
 MAX_ACK_REQUESTS = 5
 L2_WORD_SIZE = 8
-DELAY_BETWEEN_FRAGMENTS = 10  # in seconds
+DELAY_BETWEEN_FRAGMENTS = 0  # in seconds
 
 # Sender config
-ENABLE_MAX_ACK_REQUESTS = True
 
-# Receiver config
+# Disables the MAX_ACK_REQUESTS limit.
+# Default: False
+DISABLE_MAX_ACK_REQUESTS = True
+
+# ========= Receiver config
+
+# Makes SCHCReceiver delete all data from a SCHC session after reassembly.
+# Default: False
 RESET_DATA_AFTER_REASSEMBLY = False
+
+# Enables SCHCReceiver to check for Sigfox callback retries.
+# Default: True
 CHECK_FOR_CALLBACK_RETRIES = True
+
+# Disables SCHCReceiver to perform inactivity timer validation.
+# Default: False
 DISABLE_INACTIVITY_TIMEOUT = False
 
 RECEIVER_URL = "http://localhost:5000/receive"
